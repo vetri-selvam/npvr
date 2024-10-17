bbc80cac6095346d0f279e7c862729235a5ce7ec
Vetri Selvam <vetriwork1@gmail.com>
2024-10-17 19:50:36 +0530

    installed mongoDB

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const path = require('path');
+const connectDatabase = require('./config/connectDatabase');
 dotenv.config({path: path.join(__dirname, 'config', 'config.env')})
 
 const products = require('./routes/product');
 const orders = require('./routes/order');
+
+connectDatabase();
 
 app.use('/api/v1/', products);
 app.use('/api/v1/', orders);
