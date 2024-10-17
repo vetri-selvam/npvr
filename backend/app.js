3f37d6b3d3b6f728f1801bf4a5b8445f1fb27f34
Vetri Selvam <vetriwork1@gmail.com>
2024-10-17 11:52:20 +0530

    added express

---
+++ backend/app.js
@@ -0,0 +1,8 @@
+const express = require('expres');
+const app = express();
+app.listen(8000,
+        () =>{
+            console.log("Server listening to port 8000 in development");
+        }
+);
+
