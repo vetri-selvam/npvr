3ed30e6f13b1696a0efecc3c0966c7a145dfebd6
Vetri Selvam <vetriwork1@gmail.com>
2024-10-17 12:58:40 +0530

    add dotenv, routes, controllers, APIs - Added all the sample controllers like Products and Orders for sample testing - Tested them using Thunder Client and tested client APIs

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
-app.listen(8000,
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config({path: path.join(__dirname, 'config', 'config.env')})
+
+const products = require('./routes/product');
+const orders = require('./routes/order');
+
+app.use('/api/v1/', products);
+app.use('/api/v1/', orders);
+
+app.listen(process.env.PORT
+        ,
         () =>{
-            console.log("Server listening to port 8000 in development");
+            console.log(`Server listening to port ${process.env.PORT} in ${process.env.NODE_NAVEEN}`);
         }
+
 );
 
---
+++ backend/controller/productController.js
@@ -0,0 +1,17 @@
+exports.getProducts = (req, res, next) =>{
+    res.json(
+        {
+            success: true,
+            message: 'Get products working!'
+        }
+    )
+}
+
+exports.getSingleProducts = (req, res, next) =>{
+    res.json(
+        {
+            success: true,
+            message: 'Get single products working!'
+        }
+    )
+}
