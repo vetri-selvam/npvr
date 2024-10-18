3ee9d1e668cfb131cea4b287b59ddf6b520a92b7
Vetri Selvam <vetriwork1@gmail.com>
2024-10-18 12:09:23 +0530

    format change

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -1,27 +1,20 @@
-const orderModel = require("../models/orderModel")
+const orderModel = require("../models/orderModel");
 
 // Create Order - /api/v1/order
-exports.createOrder =async (req, res, next) =>{
+exports.createOrder = async (req, res, next) => {
     const cartItems = req.body;
-    const amount =Number( cartItems.reduce(
-                                (acc, item) =>
-                                    (acc+ item.product.price * item.qty)
-                                    , 0
-                            )
-                        );
-    const status = 'pending';
+    const amount = Number(
+        cartItems.reduce((acc, item) => acc + item.product.price * item.qty, 0)
+    ).toFixed(2);
+    const status = "pending";
 
-    const order = await orderModel.create(
-                {
-                    cartItems,
-                    amount,
-                    status
-                }
-    )
-    res.json(
-        {
-            success: true,
-            order
-        }
-    )
-}
+    const order = await orderModel.create({
+        cartItems,
+        amount,
+        status,
+    });
+    res.json({
+        success: true,
+        order,
+    });
+};
