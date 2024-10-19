5ee5751d563e45f7a0e6a3c8e7c1854cc9de1194
Vetri Selvam <vetriwork1@gmail.com>
2024-10-19 11:09:02 +0530

    refactor productController.js to dynamically fetch and display products

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,36 +1,34 @@
-const  productModel = require("../models/productModel");
+const productModel = require("../models/productModel");
 
 // Get Products API - /api/v1/products
-exports.getProducts = async(req, res, next) =>{
-
-    const products = await productModel.find({});  // passing empty object i.e. getting all properties
-    res.json(
-        {
-            success: true,
-            products
-            // message: 'Get products working!'
+exports.getProducts = async (req, res, next) => {
+    const query = req.query.keyword?{
+        name: {
+            $regex: req.query.keyword,
+            $options: 'i'
         }
-    )
-}
+    }:{}
+
+    const products = await productModel.find(query); // passing empty object i.e. getting all properties
+    res.json({
+        success: true,
+        products,
+        // message: 'Get products working!'
+    });
+};
 
 // Get Single Product API - /api/v1/products
-exports.getSingleProducts = async (req, res, next) =>{
-
+exports.getSingleProducts = async (req, res, next) => {
     try {
-        const product= await productModel.findById(req.params.id)
-        res.json(
-        {
+        const product = await productModel.findById(req.params.id);
+        res.json({
             success: true,
-            product
-        }
-        )
-        
+            product,
+        });
     } catch (error) {
-        res.status(404).json(
-            {
-                success: false,
-                message: 'Unable to get Product with that ID'
-            }
-            )
+        res.status(404).json({
+            success: false,
+            message: "Unable to get Product with that ID",
+        });
     }
-}
+};
