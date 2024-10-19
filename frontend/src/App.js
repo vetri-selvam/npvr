47674cad56f7aadbe07abfb4a66f4bb53b1b3242
Vetri Selvam <vetriwork1@gmail.com>
2024-10-19 13:02:16 +0530

    Refactor routing in App.js and add ProductDetails page
    - Refactor the routing in App.js to include a new route for the ProductDetails page.
    - Import the ProductDetails component in App.js.
    - Add a new Route for the '/product/:id' path in the Routes component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './pages/Home';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import ProductDetails from './pages/ProductDetails';
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />}/>
             <Route path='/search' element={<Home />}/>
+            <Route path='/product/:id' element={<ProductDetails />}/>
           </Routes>
         </div>
       </Router>   
--- frontend/src/pages/ProductDetails.js
+++ frontend/src/pages/ProductDetails.js
@@ -1,1 +1,106 @@
-export default
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+export default function ProductDetails() {
+    const [product, setProduct] = useState(null);
+    const { id } = useParams();
+
+    useEffect(() => {
+        fetch(process.env.REACT_APP_API_URL + "/products/" + id)
+            .then((res) => res.json())
+            .then((res) => setProduct(res.product));
+    }, []);
+
+    return (
+        product && (
+            <div className="container container-fluid">
+                <div className="row f-flex justify-content-around">
+                    <div
+                        className="col-12 col-lg-5 img-fluid"
+                        id="product_image"
+                    >
+                        <img
+                            src={product.images[0].image}
+                            alt="sdf"
+                            height="500"
+                            width="500"
+                        />
+                    </div>
+
+                    <div className="col-12 col-lg-5 mt-5">
+                        <h3>{product.name}</h3>
+                        <p id="product_id">Product # {product._id}</p>
+
+                        <hr />
+
+                        <div className="rating-outer">
+                            <div
+                                className="rating-inner"
+                                style={{
+                                    width: `${(product.ratings / 5) * 100}%`,
+                                }}
+                            ></div>
+                        </div>
+
+                        <hr />
+
+                        <p id="product_price">{product.price}</p>
+                        <div className="stockCounter d-inline">
+                            <span className="btn btn-danger minus">-</span>
+
+                            <input
+                                type="number"
+                                className="form-control count d-inline"
+                                value="1"
+                                readOnly
+                            />
+
+                            <span className="btn btn-primary plus">+</span>
+                        </div>
+                        <button
+                            type="button"
+                            id="cart_btn"
+                            className="btn btn-primary d-inline ml-4"
+                        >
+                            Add to Cart
+                        </button>
+
+                        <hr />
+
+                        <p>
+                            Status:{" "}
+                            <span
+                                id="stock_status"
+                                className={
+                                    product.stock > 0
+                                        ? product.stock == 1
+                                            ? "text-warning"
+                                        : "text-success"
+                                    : "text-danger"
+                                }
+                            >
+                                {" "}
+                                {product.stock > 0
+                                    ? product.stock == 1
+                                        ? "Only ONE in Stock"
+                                        : "In Stock"
+                                    : "Out of Stock"}
+                            </span>
+                        </p>
+
+                        <hr />
+
+                        <h4 className="mt-2">Description:</h4>
+                        <p>{product.description}</p>
+                        <hr />
+                        <p id="product_seller mb-3">
+                            Sold by: <strong>{product.seller}</strong>
+                        </p>
+
+                        <div className="rating w-50"></div>
+                    </div>
+                </div>
+            </div>
+        )
+    );
+}
